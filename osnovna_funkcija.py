30f099688b4ccc7669ade0443bb27e47cd423b58
Justin Raišp <justin.raisp37@gmail.com>
2022-11-28 20:34:45 +0100

    Popravil osnovno funkcijo glede na pripombe prof

--- osnovna_funkcija.py
+++ osnovna_funkcija.py
@@ -11,7 +11,8 @@ import random
 def maksimalno_stevilo_zogic(n, s=1):
     B = [0]*n
     x = 0
-    while x <= n-1:
+    print(B)
+    for x in range(n):
         izbrana_kosarica = random.randrange(n)
         if izbrana_kosarica == 0:
             sosedi = [B[-1]] + B[0:2]
@@ -21,7 +22,6 @@ def maksimalno_stevilo_zogic(n, s=1):
                 B[-1] += 1
             else:
                 B[izbrana_kosarica -1] += 1
-            x += 1
         elif izbrana_kosarica == n-1:
             sosedi = B[n-2:n] + [B[0]]
             minimalni_sosed = min(sosedi)
@@ -30,10 +30,30 @@ def maksimalno_stevilo_zogic(n, s=1):
                 B[0] += 1
             else:
                 B[izbrana_kosarica-2] +=1
-            x += 1
         else:
             minimalni_sosed = min(B[izbrana_kosarica -1 : izbrana_kosarica +2])
             indeks = B[izbrana_kosarica - 1 : izbrana_kosarica + 2].index(minimalni_sosed)
             B[izbrana_kosarica -1 + indeks] += 1
-            x += 1
+    print(B)
     return max(B)
+
+
+
+
+def maksimalno_stevilo_zogic2(st_zogic, st_kosov, razdalja=1):
+    kosi = [0]*st_kosov
+    for i in range(st_zogic):
+        izbrana_kosarica = random.randrange(st_kosov)
+        sosedi = {}
+        for j in range((-razdalja),(razdalja+1)):
+            indeks_soseda = (izbrana_kosarica + j) % st_kosov
+            sosedi[indeks_soseda] = kosi[indeks_soseda]
+            min_vrednost = min(sosedi.values())
+        kandidati = [k for k,v in sosedi.items() if v == min_vrednost]
+        min_sosed = min(kandidati)
+        kosi[min_sosed] += 1
+        print(izbrana_kosarica)
+        print(sosedi)
+        print(kandidati)
+        print(kosi)
+maksimalno_stevilo_zogic2(10,10,4)
