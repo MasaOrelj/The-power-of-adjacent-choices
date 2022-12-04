f2572e598b36c497ce09c3f852e31420618ced93
Maša Orelj <you@example.com>
2022-12-04 21:07:03 +0100

    Slovnični popravki

--- analiza.r
+++ analiza.r
@@ -40,14 +40,9 @@ minimumi_st_zog_1dim_1 <- rbind(dim1_10000_10000_1_min, dim1_20000_10000_1_min, 
 
 #Vizualizacija
 
-
-
-#stevila <- rep(1:100, 5)
 maks_zdruzena_dim1_zogice <- cbind(dim1_10000_10000_1[1], dim1_20000_10000_1[1], dim1_30000_10000_1[1], dim1_40000_10000_1[1],dim1_50000_10000_1[1])
 colnames(maks_zdruzena_dim1_zogice) <- c("maks_10000", "maks_20000", "maks_30000", "maks_40000", "maks_50000")
-#maks_zdruzena_dim1_zogice_preurejena <- maks_zdruzena_dim1_zogice %>% pivot_longer(c(1:5), names_to = "colname", values_to = "val") %>% arrange(colname)
-#maks_zdruzena_dim1_zogice_preurejena <- cbind(stevila, maks_zdruzena_dim1_zogice_preurejena)
-#
+
 povp1 <- apply(maks_zdruzena_dim1_zogice, 2, mean)
 #
 graf_povp_1 <- enframe(povp1) %>% ggplot(
@@ -62,43 +57,6 @@ graf_povp_1 <- enframe(povp1) %>% ggplot(
     title = "Povprečno maksimalno število žog v košu glede na število žog"
   ) 
 graf_povp_1
-
-#
-#graf1 <- maks_zdruzena_dim1_zogice_preurejena %>% filter(colname %in% maks_zdruzena_dim1_zogice_preurejena$colname)%>%
-#  ggplot(
-#    mapping = aes(x = stevila, y = val)
-#  )+
-#  geom_point()+
-#  geom_smooth(method = lm, size=1)+
-#  facet_wrap(. ~ colname)
-#
-#graf1
-#
-#
-#graf1 <- ggplot(
-#    mapping = aes(x = c(1:100), y = maks_zdruzena_dim1_zogice$maks_10000)
-#  )+
-#  geom_point()+
-#  geom_smooth(method = lm, size=1)+
-#  geom_point(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_20000))+
-#  geom_smooth(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_20000),method = lm, size=1)+
-#  geom_point(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_30000))+
-#  geom_smooth(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_30000),method = lm, size=1)+
-#  geom_point(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_40000))+
-#  geom_smooth(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_40000),method = lm, size=1)+
-#  geom_point(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_50000))+
-#  geom_smooth(aes(x = c(1:100), maks_zdruzena_dim1_zogice$maks_50000),method = lm, size=1)
-#graf1
-#
-#preizkus histograma
-#par(mfrow=c(1,1))
-#maksimum_dim1_1000_1000_1 <- as.numeric(unlist(dim1_10000_10000_1[1]))
-#h_dim1_1000_1000_1 <- hist(maksimum_dim1_1000_1000_1, xlim = c(min(maksimum_dim1_1000_1000_1)-0.05, max(maksimum_dim1_1000_1000_1)+0.05), ylim = c(0,25))
-#abline(v = mean(maksimum_dim1_1000_1000_1), col = "red", lty = "dashed")
-#text(x = mean(maksimum_dim1_1000_1000_1), y = 25, label = round(mean(maksimum_dim1_1000_1000_1),3), pos=2, col="red")
-#legend('topright', legend = c("Povprečje"),
-#       col = c("red"), cex=0.6, lty=c("dashed"))
-
 
 par(mfrow=c(3,1))
 
@@ -146,7 +104,7 @@ graf_povp_2 <- enframe(povp2) %>% ggplot(
   labs(
     x = "Razdalja",
     y = "Povprečno maksimalno število žog",
-    title = "Povprečno maksimalno število žog v košu glede razdaljo"
+    title = "Povprečno maksimalno število žog v košu glede na razdaljo"
   ) 
 graf_povp_2
 
@@ -154,11 +112,15 @@ graf_povp_2
 par(mfrow=c(3,1))
 plot(x=c(1:500), maksimumi_st_zog_1dim_k$Maksimum, type="p", pch=20, cex=0.5, main = "Maksimalna vrednost glede na razdaljo", ylab = "Maksimalno število žog v košu", xlab = "Razdalja", xaxt = "n")
 axis(1, at=seq(50, 450, by=100), labels = c(1, 2, 3, 4, 5))
-plot(x=c(1:500), maksimumi_st_zog_1dim_k$Delez_kosev_z_maksimalnim_stevilom_zog, type="p", pch=20, cex=0.5, main = "Delež košev z maksimalno vrednostjo glede razdaljo", ylab = "Delež košev z maksimlanim številom žog", xlab = "Razdalja", xaxt = "n")
+plot(x=c(1:500), maksimumi_st_zog_1dim_k$Delez_kosev_z_maksimalnim_stevilom_zog, type="p", pch=20, cex=0.5, main = "Delež košev z maksimalno vrednostjo glede na razdaljo", ylab = "Delež košev z maksimlanim številom žog", xlab = "Razdalja", xaxt = "n")
 axis(1, at=seq(50, 450, by=100), labels = c(1, 2, 3, 4, 5))
 plot(x=c(1:500), maksimumi_st_zog_1dim_k$Casovna_zahtevnost, type="p", pch=20, cex=0.5, main = "Časovna zahtevnost glede na razdaljo", ylab = "Časovna zahtevnost", xlab = "Razdalja", xaxt = "n")
 axis(1, at=seq(50, 450, by=100), labels = c(1, 2, 3, 4, 5))
 
+
+graf_povp_1dim <-ggarrange(graf_povp_1, graf_povp_2, 
+                           ncol = 2, nrow = 1)
+graf_povp_1dim
 
 ############################################################
 # dim = 2, stevilo_zog = k*10000 (k=1,2,3,4,5), razdalja = 1
@@ -186,9 +148,9 @@ graf_povp_3 <- enframe(povp3) %>% ggplot(
   geom_point(size = 3)+
   geom_label_repel(aes(label = value))+
   labs(
-    x = "Število žog (v 10000",
+    x = "Število žog (v 10000)",
     y = "Povprečno maksimalno število žog",
-    title = "Povprečno maksimalno število žog v košu glede število žog"
+    title = "Povprečno maksimalno število žog v košu glede na število žog"
   ) 
 graf_povp_3
 
@@ -226,7 +188,7 @@ graf_povp_4 <- enframe(povp4) %>% ggplot(
   labs(
     x = "Razdalja",
     y = "Povprečno maksimalno število žog",
-    title = "Povprečno maksimalno število žog v košu glede razdaljo"
+    title = "Povprečno maksimalno število žog v košu glede na razdaljo"
   ) 
 graf_povp_4
 
@@ -238,6 +200,11 @@ axis(1, at=seq(50, 450, by=100), labels = c(1, 2, 3, 4, 5))
 plot(x=c(1:500), maksimumi_st_zog_2dim_k$Casovna_zahtevnost, type="p", pch=20, cex=0.5, main = "Časovna zahtevnost glede na razdaljo", ylab = "Časovna zahtevnost", xlab = "Razdalja", xaxt = "n")
 axis(1, at=seq(50, 450, by=100), labels = c(1, 2, 3, 4, 5))
 
+
+
+graf_povp_2dim <-ggarrange(graf_povp_3, graf_povp_4, 
+                           ncol = 2, nrow = 1)
+graf_povp_2dim
 
 
 ###################################################################
